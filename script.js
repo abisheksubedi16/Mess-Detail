a50ecc8c8a412d02233d19e35e6cfbdd82de639e
abisheksubedi16 <87187274+abisheksubedi16@users.noreply.github.com>
2023-05-01 15:21:24 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('thismess').innerHTML = `...............................................................................`;
+
+function abc1(x){
+    document.getElementById('thismess').innerHTML =`What are Mess's "Front-End" & "Back-End"?`;
+}
+function def1(x){
+    document.getElementById('thismess').innerHTML =`...............................................................................`;
+}
+function abc2(x){
+    document.getElementById('thismess').innerHTML =`What are the thing that "annoy" Mess ? And how Mess's annoy people ?`;
+}
+function def2(x){
+    document.getElementById('thismess').innerHTML =`...............................................................................`;
+}
